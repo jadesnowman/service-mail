37efbf2649c265fc5b3944038853017b4ed61424
Ilham Saputrajati <ilhamsaputrajati@gmail.com>
2021-10-16 00:13:51 +0700

    validation

--- app/Http/Controllers/V1/MailController.php
+++ app/Http/Controllers/V1/MailController.php
@@ -3,35 +3,18 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
-use App\Mail\MailService;
-use App\Traits\JsonResponse;
+use App\Http\Services\Logics\V1\SendingEmail;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
-    use JsonResponse;
+    public function __construct(SendingEmail $logic)
+    {
+        $this->logic = $logic;
+    }
 
     public function sendEmail(Request $request)
     {
-        try {
-            $this->validate($request, [
-                'from'          => 'required|email',
-                'to'            => 'required|email',
-                'subject'       => 'required',
-                'content'       => 'required',
-            ]);
-
-            $data = new \StdClass;
-            $data->from = $request->from;
-            $data->to = $request->to;
-            $data->subject = $request->subject;
-            $data->content = $request->content;
-
-            Mail::to($request->to)->send(new MailService($data));
-            return $this->succcess('Sending email!', $data);
-        } catch (\Exception $exception) {
-            return $this->handleErrorMessage($exception);
-        }
+        return $this->logic->send($request);
     }
 }
